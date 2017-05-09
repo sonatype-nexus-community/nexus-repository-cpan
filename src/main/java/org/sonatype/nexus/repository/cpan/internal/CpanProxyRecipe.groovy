c4bdcbafef0286c0715f4d0a5496290ed300fd3e
ryan-sheldrake <ryansheldrake@hotamil.com>
2017-05-09 11:47:10 -0400

    Public export of Nexus Repository - CPAN Plugin:
      - 730698bcadf4c6aa010e30e1e59686d15dcf4ba6 by ryan-sheldrake <ryansheldrake@hotamil.com>
    
    GitOrigin-RevId: 730698bcadf4c6aa010e30e1e59686d15dcf4ba6

--- src/main/java/org/sonatype/nexus/repository/cpan/internal/CpanProxyRecipe.groovy
+++ src/main/java/org/sonatype/nexus/repository/cpan/internal/CpanProxyRecipe.groovy
@@ -13,24 +13,95 @@
 package org.sonatype.nexus.repository.cpan.internal
 
 import org.sonatype.nexus.repository.Format
+import org.sonatype.nexus.repository.RecipeSupport
+import org.sonatype.nexus.repository.Repository
+import org.sonatype.nexus.repository.attributes.AttributesFacet
 import org.sonatype.nexus.repository.cache.NegativeCacheFacet
 import org.sonatype.nexus.repository.cache.NegativeCacheHandler
+import org.sonatype.nexus.repository.http.PartialFetchHandler
+import org.sonatype.nexus.repository.httpclient.HttpClientFacet
 import org.sonatype.nexus.repository.proxy.ProxyHandler
+import org.sonatype.nexus.repository.search.SearchFacet
+import org.sonatype.nexus.repository.security.SecurityHandler
+import org.sonatype.nexus.repository.storage.SingleAssetComponentMaintenance
+import org.sonatype.nexus.repository.storage.StorageFacet
+import org.sonatype.nexus.repository.storage.UnitOfWorkHandler
 import org.sonatype.nexus.repository.types.ProxyType
 import org.sonatype.nexus.repository.Type
+import org.sonatype.nexus.repository.view.ConfigurableViewFacet
+import org.sonatype.nexus.repository.view.Route
+import org.sonatype.nexus.repository.view.Router
+import org.sonatype.nexus.repository.view.ViewFacet
+import org.sonatype.nexus.repository.view.handlers.BrowseUnsupportedHandler
+import org.sonatype.nexus.repository.view.handlers.ConditionalRequestHandler
+import org.sonatype.nexus.repository.view.handlers.ContentHeadersHandler
+import org.sonatype.nexus.repository.view.handlers.ExceptionHandler
+import org.sonatype.nexus.repository.view.handlers.HandlerContributor
+import org.sonatype.nexus.repository.view.handlers.TimingHandler
 
+import javax.annotation.Nonnull
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Provider
-
+import javax.inject.Singleton
 
 
 /**
 * CPAN proxy repository recipe
  */
 @Named(CpanProxyRecipe.NAME)
-class CpanProxyRecipe {
+@Singleton
+class CpanProxyRecipe
+    extends RecipeSupport
+{
     public static final String NAME = 'cpan-proxy'
+
+    //@Inject
+    //Provider<RSecurityFacet> securityFacet
+    @Inject
+    Provider<ConfigurableViewFacet> viewFacet
+
+    @Inject
+    Provider<StorageFacet> storageFacet
+
+    @Inject
+    Provider<SearchFacet> searchFacet
+
+    @Inject
+    Provider<AttributesFacet> attributesFacet
+
+    @Inject
+    ExceptionHandler exceptionHandler
+
+    @Inject
+    TimingHandler timingHandler
+
+    @Inject
+    SecurityHandler securityHandler
+
+    @Inject
+    PartialFetchHandler partialFetchHandler
+
+    @Inject
+    ConditionalRequestHandler conditionalRequestHandler
+
+    @Inject
+    ContentHeadersHandler contentHeadersHandler
+
+    @Inject
+    UnitOfWorkHandler unitOfWorkHandler
+
+    @Inject
+    BrowseUnsupportedHandler browseUnsupportedHandler
+
+    @Inject
+    HandlerContributor handlerContributor
+
+    @Inject
+    Provider<SingleAssetComponentMaintenance> componentMaintenanceFacet
+
+    @Inject
+    Provider<HttpClientFacet> httpClientFacet
 
     @Inject
     ProxyHandler proxyHandler
@@ -45,5 +116,33 @@ class CpanProxyRecipe {
     CpanProxyRecipe(@Named(ProxyType.NAME) final Type type,
     @Named(CpanFormat.NAME) final Format format) {
         super(type, format)
+    }
+
+    @Override
+    void apply(@Nonnull final Repository repository) throws Exception {
+        //repository.attach(securityFacet.get())
+        repository.attach(configure(viewFacet.get()))
+        repository.attach(httpClientFacet.get())
+        repository.attach(negativeCacheFacet.get())
+        repository.attach(componentMaintenanceFacet.get())
+        //repository.attach(proxyFacet.get())
+        repository.attach(storageFacet.get())
+        repository.attach(searchFacet.get())
+        repository.attach(attributesFacet.get())
+    }
+
+    /**
+     * Configure {@link ViewFacet}.
+     */
+    private ViewFacet configure(final ConfigurableViewFacet facet) {
+        Router.Builder builder = new Router.Builder()
+
+        builder.route(new Route.Builder()
+                .matcher(BrowseUnsupportedHandler.MATCHER)
+                .handler(browseUnsupportedHandler)
+                .create())
+        builder.defaultHandlers(HttpHandlers.notFound())
+        facet.configure(builder.create())
+        return facet
     }
 }
