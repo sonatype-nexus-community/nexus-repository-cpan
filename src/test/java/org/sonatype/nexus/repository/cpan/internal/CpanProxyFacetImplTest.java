b035a12e8123b934b61e1c3750df751d545ce46c
DarthHater <jeffryxtron@gmail.com>
2017-08-08 19:31:15 -0700

    Public export of Nexus Repository - CPAN Plugin:
      - 343f0bc0d362a588f0fda529287ab2b380d3c3ff by DarthHater <jeffryxtron@gmail.com>
    
    GitOrigin-RevId: 343f0bc0d362a588f0fda529287ab2b380d3c3ff

---
+++ src/test/java/org/sonatype/nexus/repository/cpan/internal/CpanProxyFacetImplTest.java
@@ -0,0 +1,58 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.cpan.internal;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.repository.view.Context;
+import org.sonatype.nexus.repository.view.Request;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for {@link CpanProxyFacetImpl}
+ */
+public class CpanProxyFacetImplTest
+    extends TestSupport
+{
+  private CpanProxyFacetImpl underTest;
+  private CpanParser cpanParser;
+
+  @Mock
+  Context context;
+
+  @Mock
+  Request request;
+
+  @Before
+  public void setUp() throws Exception {
+    cpanParser = new CpanParser();
+    underTest = new CpanProxyFacetImpl(cpanParser);
+  }
+
+  @Test
+  public void getUrl() throws Exception {
+    String testUrl = "/repository/cpan-proxy";
+    String expectedUrl = testUrl.substring(1);
+
+    when(context.getRequest()).thenReturn(request);
+    when(request.getPath()).thenReturn(testUrl);
+
+    assertThat(underTest.getUrl(context), is(expectedUrl));
+  }
+}
