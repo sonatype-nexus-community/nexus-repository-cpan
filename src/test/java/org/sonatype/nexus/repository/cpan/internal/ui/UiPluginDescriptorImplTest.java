0bbba5ff37eee5f76a6aed690cab1a7225268bc8
DarthHater <jeffryxtron@gmail.com>
2017-08-08 19:04:21 -0700

    Public export of Nexus Repository - CPAN Plugin:
      - 5ee72c5d1019db2dafa90da80ffa4efd5362fbdb by DarthHater <jeffryxtron@gmail.com>
    
    GitOrigin-RevId: 5ee72c5d1019db2dafa90da80ffa4efd5362fbdb

--- src/test/java/org/sonatype/nexus/repository/cpan/internal/ui/UiPluginDescriptorImplTest.java
+++ src/test/java/org/sonatype/nexus/repository/cpan/internal/ui/UiPluginDescriptorImplTest.java
@@ -1,9 +1,29 @@
-import static org.junit.Assert.*;
+package org.sonatype.nexus.repository.cpan.internal.ui;
 
-/**
- * Created by jeffryhesse on 8/8/17.
- */
+import org.sonatype.goodies.testsupport.TestSupport;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class UiPluginDescriptorImplTest
+    extends TestSupport
 {
+  private static final String NAMESPACE = "NX.cpanui";
+  private static final String CONFIG_CLASS_NAME = "NX.cpanui.app.PluginConfig";
 
+  private UiPluginDescriptorImpl underTest;
+
+  @Before
+  public void setup() {
+    underTest = new UiPluginDescriptorImpl();
+  }
+
+  @Test
+  public void checkUiPluginDescriptorValues() {
+    assertThat(underTest.getNamespace(), is(NAMESPACE));
+    assertThat(underTest.getConfigClassName(), is(CONFIG_CLASS_NAME));
+  }
 }
