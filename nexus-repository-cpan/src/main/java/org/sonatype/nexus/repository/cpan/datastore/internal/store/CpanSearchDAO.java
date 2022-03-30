341793c1123525f00a2f396c5722ba117547f8b9
christophvw <Christoph_vW@ReactOS.org>
2022-03-30 11:54:49 +0200

    Update CpanSearchDAO.java
    fix namespace

--- nexus-repository-cpan/src/main/java/org/sonatype/nexus/repository/cpan/datastore/internal/store/CpanSearchDAO.java
+++ nexus-repository-cpan/src/main/java/org/sonatype/nexus/repository/cpan/datastore/internal/store/CpanSearchDAO.java
@@ -10,7 +10,8 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.content.example.internal.store;
+package org.sonatype.nexus.repository.cpan.datastore.internal.store;
+
 
 import org.sonatype.nexus.repository.content.search.SearchDAO;
 
